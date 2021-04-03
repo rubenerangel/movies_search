9e489b552e904e42eadca863934356f277752fdc
Ruben Rangel <rubenrang@gmail.com>
2021-04-02 23:14:27 -0500

    Model and Migration

---
+++ migrations/20210403034428-create-movie.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('movies_ruben', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.STRING(6)
+      },
+      mdbID: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING(8)
+      },
+      poster: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('movies');
+  }
+};
---
+++ models/movie.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class movie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  movie.init({
+    title: DataTypes.STRING,
+    year: DataTypes.STRING,
+    mdbID: DataTypes.STRING,
+    type: DataTypes.STRING,
+    poster: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'movie',
+    tableName: 'movies_ruben'
+  });
+  return movie;
+};
